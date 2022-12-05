b0ee9e30bab7d1989b6c731fb89e59dc0155a741
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-12-05 11:37:00 -0500

    Added Portfolio Item Descriptions Changed the href of item pointing to home to /# instead of #

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,53 +1,55 @@
-import React from "react";
-import { FaFacebookF } from "react-icons/fa";
-import { FiInstagram } from "react-icons/fi";
-import { IoLogoTwitter } from "react-icons/io";
-import { BsLinkedin } from "react-icons/bs";
-import "./footer.css";
+import React from 'react';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
+import { BsLinkedin } from 'react-icons/bs';
+import './footer.css';
 
 const Footer = () => {
-  return (
-    <footer>
-      <a href="#" className="footer__logo">
-        JH
-      </a>
+	return (
+		<footer>
+			<a
+				href='/#'
+				className='footer__logo'>
+				JH
+			</a>
 
-      <ul className="permalinks">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-      <div className="footer__socials">
-        <a href="facebook.com">
-          <FaFacebookF />
-        </a>
-        <a href="Instagram.com">
-          <FiInstagram />
-        </a>
-        <a href="Twitter.com">
-          <IoLogoTwitter />
-        </a>
-        <a href="Linkedin.com">
-          <BsLinkedin />
-        </a>
-      </div>
-      <div className="footer__copyright">
-        <small>&copy; Joseph Huntley. All rights reserved.</small>
-      </div>
-    </footer>
-  );
+			<ul className='permalinks'>
+				<li>
+					<a href='/#'>Home</a>
+				</li>
+				<li>
+					<a href='#about'>About</a>
+				</li>
+				<li>
+					<a href='#experience'>Experience</a>
+				</li>
+				<li>
+					<a href='#portfolio'>Portfolio</a>
+				</li>
+				<li>
+					<a href='#contact'>Contact</a>
+				</li>
+			</ul>
+			<div className='footer__socials'>
+				<a href='facebook.com'>
+					<FaFacebookF />
+				</a>
+				<a href='Instagram.com'>
+					<FiInstagram />
+				</a>
+				<a href='Twitter.com'>
+					<IoLogoTwitter />
+				</a>
+				<a href='Linkedin.com'>
+					<BsLinkedin />
+				</a>
+			</div>
+			<div className='footer__copyright'>
+				<small>&copy; Joseph Huntley. All rights reserved.</small>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,104 +1,118 @@
-import React from "react";
-import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
 
 const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: "HRIS",
-    github: "github.com",
-    demo: "github.com",
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: "Memories",
-    github: "github.com",
-    demo: "github.com",
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: "React",
-    github: "github.com",
-    demo: "github.com",
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: "Cocktail Nation",
-    github: "https://github.com/JosephHuntley/cocktailNation",
-    demo: "github.com",
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: "Angular",
-    github: "github.com",
-    demo: "github.com",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "React",
-    github: "github.com",
-    demo: "github.com",
-  },
+	{
+		id: 1,
+		image: IMG1,
+		title: 'HRIS',
+		github: 'github.com',
+		demo: 'github.com',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fugiat cumque nostrum atque dolorum laboriosam!',
+	},
+	{
+		id: 2,
+		image: IMG2,
+		title: 'Memories',
+		github: 'github.com',
+		demo: 'github.com',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fugiat cumque nostrum atque dolorum laboriosam!',
+	},
+	{
+		id: 3,
+		image: IMG3,
+		title: 'Gym Exercises',
+		github: 'https://github.com/JosephHuntley/gym_exercises',
+		demo: 'https://youtube.com/playlist?list=PLGneXjfBUnOGLQfg-qrsUrOohGdqSmE1p',
+		description:
+			'A reponsive webpage that uses exerciseDB and Youtube Search from RapidAPI to show users popular exercises, and videos on how to preform them.',
+	},
+	{
+		id: 4,
+		image: IMG4,
+		title: 'Cocktail Nation',
+		github: 'https://github.com/JosephHuntley/cocktailNation',
+		demo: 'github.com',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fugiat cumque nostrum atque dolorum laboriosam!',
+	},
+	{
+		id: 5,
+		image: IMG5,
+		title: 'Angular',
+		github: 'github.com',
+		demo: 'github.com',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fugiat cumque nostrum atque dolorum laboriosam!',
+	},
+	{
+		id: 6,
+		image: IMG6,
+		title: 'React',
+		github: 'github.com',
+		demo: 'github.com',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos fugiat cumque nostrum atque dolorum laboriosam!',
+	},
 ];
 
 const Portfolio = () => {
-  return (
-    <section id="portfolio">
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
-      <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
-          return (
-            <article key={id} className="portfolio__item flip-card">
-              <div className="flip-card-inner">
-                <div className="flip-card-front">
-                  <div className="card-body">
-                    <div className="portfolio__item-image">
-                      <img src={image} alt={title} />
-                    </div>
-                    <h3 className="portfolio__title">{title}</h3>
-                  </div>
-                </div>
-                <div className="flip-card-back card-body">
-                  <h3 className="card-description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quos fugiat cumque nostrum atque dolorum laboriosam!
-                  </h3>
-                  <div className="portfolio__item-cta">
-                    <a
-                      href={github}
-                      className="btn"
-                      target="_blank"
-                      rel="noreferrer">
-                      GitHub
-                    </a>
-                    <a
-                      href={demo}
-                      className="btn btn-primary"
-                      target="_blank"
-                      rel="noreferrer">
-                      Demo
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </article>
-          );
-        })}
-      </div>
-    </section>
-  );
+	return (
+		<section id='portfolio'>
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
+			<div className='container portfolio__container'>
+				{data.map(({ id, image, title, github, demo, description }) => {
+					return (
+						<article
+							key={id}
+							className='portfolio__item flip-card'>
+							<div className='flip-card-inner'>
+								<div className='flip-card-front'>
+									<div className='card-body'>
+										<div className='portfolio__item-image'>
+											<img
+												src={image}
+												alt={title}
+											/>
+										</div>
+										<h3 className='portfolio__title'>{title}</h3>
+									</div>
+								</div>
+								<div className='flip-card-back card-body'>
+									<h3 className='card-description'>{description}</h3>
+									<div className='portfolio__item-cta'>
+										<a
+											href={github}
+											className='btn'
+											target='_blank'
+											rel='noreferrer'>
+											GitHub
+										</a>
+										<a
+											href={demo}
+											className='btn btn-primary'
+											target='_blank'
+											rel='noreferrer'>
+											Demo
+										</a>
+									</div>
+								</div>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
