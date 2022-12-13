e5402840c9f7bb61d003c7a1c09822653ef337cd
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-12-13 11:39:53 -0500

    Implement sanity.ignored

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,56 +1,68 @@
-import React from "react";
-import "./about.css";
-import ME from "../../assets/me-about.jpg";
-import { FaAward } from "react-icons/fa";
-import { FiUsers } from "react-icons/fi";
-import { VscFolderLibrary } from "react-icons/vsc";
+import React from 'react';
+import './about.css';
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
+import { SlHourglass } from 'react-icons/sl';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
-  return (
-    <section id="about">
-      <h5 className="">Get to know</h5>
-      <h2>About me</h2>
+	return (
+		<section id='about'>
+			<h5 className=''>Get to know</h5>
+			<h2>About me</h2>
 
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={ME} alt="Headshot" />
-          </div>
-        </div>
+			<div className='container about__container'>
+				<div className='about__me'>
+					<div className='about__me-image'>
+						<img
+							src={ME}
+							alt='Head shot'
+						/>
+					</div>
+				</div>
 
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <FaAward className="about__icon" />
-              <h5>Experience</h5>
-              <small>3+ Years</small>
-            </article>
-            <article className="about__card">
-              <FiUsers className="about__icon" />
-              <h5>Clients</h5>
-              <small>300+ Clients</small>
-            </article>
-            <article className="about__card">
-              <VscFolderLibrary className="about__icon" />
-              <h5>Projects</h5>
-              <small>80+ Completed Projects</small>
-            </article>
-          </div>
+				<div className='about__content'>
+					<div className='about__cards'>
+						<article className='about__card'>
+							<FaAward className='about__icon' />
+							<h5>Experience</h5>
+							<small>3+ Years</small>
+						</article>
+						<article className='about__card'>
+							<SlHourglass className='about__icon' />
+							<h5>Hours Coding</h5>
+							<small>300+ Hours</small>
+						</article>
+						<article className='about__card'>
+							<VscFolderLibrary className='about__icon' />
+							<h5>Projects</h5>
+							<small>80+ Completed Projects</small>
+						</article>
+					</div>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-            animi illum. Facere labore laudantium suscipit, reprehenderit iure
-            laborum in est officiis molestias illo quam porro exercitationem eum
-            omnis nemo voluptatem!
-          </p>
+					<p>
+						A Front End Developer with over three years of experience.
+						Experience with HTML, CSS, JavaScript and various front end
+						technologies like ReactJS, Redux, NextJS, TypeScript, and design
+						frameworks such as Material UI, Bootstrap, and tailwind.
+						<br />
+						<br />I have built several web applications using Rest APIs and
+						Single Page Applications (SPA) Architecture. I have some experience
+						building full stack applications using the popular tech stack MERN
+						and backend technologies like NodeJS, ExpressJS, Java Spring Boot,
+						ASP.DOTNET, and both SQL database like MySQL, Postgres and No-SQL
+						like MongoDB.
+					</p>
 
-          <a href="#contact" className="btn btn-primary">
-            Let's Talk
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+					<a
+						href='#contact'
+						className='btn btn-primary'>
+						Let's Talk
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import CTA from './CTA'
-import ME from '../../assets/me.png'
-import HeaderSocials from './HeaderSocials'
-import './header.css'
+import React from 'react';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
+import './header.css';
 
 const Header = () => {
-  return (
-    <header>
-      <div className="container header__container">
-      <h5> Hello, I'm</h5>
-      <h1>Joseph Huntley</h1>
-      <h5 className="text-light">Fullstack Developer</h5>
-      <CTA />
-      <HeaderSocials/>
+	return (
+		<header>
+			<div className='container header__container'>
+				<h5> Hello, I'm</h5>
+				<h1>Joseph Huntley</h1>
+				<h5 className='text-light'>Fullstack Developer</h5>
+				<CTA />
+				<HeaderSocials />
 
-      <div className="me">
-        <img src={ME} alt="" className="" />
-      </div>
+				<div className='me'>
+					<img
+						src={ME}
+						alt=''
+						className=''
+					/>
+				</div>
 
-      <a href="#contact" className="scroll__down">Scroll Down</a>
-      </div>
-    </header>
-  )
-}
+				<a
+					href='#contact'
+					className='scroll__down'>
+					Scroll Down
+				</a>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
