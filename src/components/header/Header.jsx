f098b89abd95379bd591ba7eb23f22ba00b7b6b2
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-12-13 15:43:32 -0500

    Changed Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,11 +8,22 @@ const Header = () => {
 	return (
 		<header>
 			<div className='container header__container'>
-				<h5> Hello, I'm</h5>
+				<h1 class='word'>
+					<span class='char'>W</span>
+					<span class='char'>E</span>
+					<span class='char'>L</span>
+					<span class='char'>C</span>
+					<span class='char'>O</span>
+					<span class='char'>M</span>
+					<span class='char'>E</span>
+				</h1>
+
+				<h5>I'm</h5>
 				<h1>Joseph Huntley</h1>
 				<h5 className='text-light'>Fullstack Developer</h5>
 				<CTA />
-				<HeaderSocials />
+
+				{/* <HeaderSocials />
 
 				<div className='me'>
 					<img
@@ -26,7 +37,7 @@ const Header = () => {
 					href='#contact'
 					className='scroll__down'>
 					Scroll Down
-				</a>
+				</a> */}
 			</div>
 		</header>
 	);
